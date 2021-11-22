35b39818f5539dd5dbcac004ccd4b0b8de94279a
Sebastian Malton <sebastian@malton.name>
2021-11-22 08:23:58 -0500

    Persist apiVersion when editing resources in monaco
    - Use a new custom k8slens prefixed label
    
    - Means that users aren't surprised when they use lens to update a
      resource to a new apiVersionWithGroup
    
    - Doesn't touch the versions in the stores
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/+config-autoscalers/horizontal-pod-autoscaler.store.ts
+++ src/renderer/components/+config-autoscalers/horizontal-pod-autoscaler.store.ts
@@ -4,7 +4,7 @@
  */
 
 import { KubeObjectStore } from "../../../common/k8s-api/kube-object.store";
-import { HorizontalPodAutoscaler, hpaApi } from "../../../common/k8s-api/endpoints/hpa.api";
+import { HorizontalPodAutoscaler, hpaApi } from "../../../common/k8s-api/endpoints/horizontal-pod-autoscaler.api";
 import { apiManager } from "../../../common/k8s-api/api-manager";
 
 export class HPAStore extends KubeObjectStore<HorizontalPodAutoscaler> {
